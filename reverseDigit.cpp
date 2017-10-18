6847a1259183ff704179d316bd4127eed594c2d6
benyammengesha <benilemma@gmail.com>
2017-10-18 00:16:50 -0400

    Update reverseDigit.cpp

--- reverseDigit.cpp
+++ reverseDigit.cpp
@@ -1,30 +1,43 @@
  
+//PREPARED BY BENYAM MENGESHA
+// A RECURSIVE PROGRAM THAT PRINT THE REVERSE OF AN INTEGER NUMBER
 #include <iostream>
-#include <cassert>
- 
+#include <math.h>
+
 using namespace std;
-
-
+//FUNCTION DEFINITION OF THE RECURSIVE FUNCTION
+//THE FUNCTION HAS A PARAMETER OF INTEGER AND OUTPUT THE REVESE OF THE INPUT
 void reverseDigits(int number){
-	if (number == 0 )  
-		return ;
-	else{
-		if (number % 10) < 0
-			num = -num;
-			cout << number % 10;
-		else
-			cout << number % 10;
-		reverseDigits  (number / 10);  
-		
-	}
+	 
+// THE BASE CASE OF THE FUNCTION 	
+if (number == 0)
+return ;
+//CHECKS WHETHER THE NUMBER IS NEGATIVE  OR NOT 
+//FOR NEGATIVE NUMBER TAKE ABSOLUTE VALUE OF MODE DIVISION BY NEGATIVE TEN 
+//IT PRINT OUT FIRST THE LAST DIGIT OF THE NUMBER 
+//CONCATINATE THE OUTPUT WITH  MINUS SIGN FOR NEGATIVE NUMBERS
+else if (number < 0){
+	cout << "-"<<abs( number%-10);
+	return reverseDigits(number / -10);
 }
+//FUNCTION CALL FOR POSITIVE NUMBERS
+else {
+	cout << number % 10;
+
+	return reverseDigits(number / 10);
+}
+
+}
+
+
 int main(){
-	int num,x;
+	int num;
 	cout << "enter a number" << endl;
+	//ACCEPTS INPUT FRON THE USER AND PUT IT IN VARIABLE
 	cin >> num;
-	 
-	  reverseDigits(num);
-	 
+	//CALL THE RECURSIVE FUNCTION PASSING THE INPUT
+	cout << "THE REVERSE OF THE NUMBER: " << endl;
+    reverseDigits(num);
 	 
 	cin.clear();
 	cin.ignore();
@@ -32,3 +45,4 @@ int main(){
 
 	return 0;
 }
+ 
