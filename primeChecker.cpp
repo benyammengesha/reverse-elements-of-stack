528176d684746c1cdab401c8b6441f0cf3ad14a3
benyammengesha <benilemma@gmail.com>
2017-10-17 22:43:07 -0400

    Add files via upload

---
+++ primeChecker.cpp
@@ -0,0 +1,39 @@
+  //a functtion that check whether a integer number is prime or not 
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+ 
+bool primeChecker(int number,int i=2){
+	 
+	if (number == i)
+		return 1;
+	 
+	 if  (number % i  == 0)
+		 
+		return  0;
+	 else
+ 
+	 return primeChecker( number,i++ );
+	
+
+}
+
+
+int main(){
+	int num;
+	cout << "enter a number" << endl;
+	 
+	cin >> num;
+	 bool x=primeChecker(num);
+	 if (x)
+		 cout << "the number is prime" << endl;
+	 else
+		 cout << "the number is not prime" << endl;
+
+	cin.clear();
+	cin.ignore();
+	cin.get();
+
+	return 0;
+}
