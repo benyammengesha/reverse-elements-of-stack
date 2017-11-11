871b2ece747b50e372cc48c272777f7a9e179895
benyammengesha <benilemma@gmail.com>
2017-11-11 16:01:23 -0500

    Update dateType.h

--- dateType.h
+++ dateType.h
@@ -77,7 +77,7 @@ void dateType::setYear(int y){
 
 
 	bool dateType::isLeapYear(int y){
-		if ((y % 4 == 0 && y % 100 == 0) || (y % 400 == 0))
+		if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
 			return true;
 		else
 			return false;
